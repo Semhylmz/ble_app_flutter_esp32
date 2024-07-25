95ec863084d4aef6327c280f29bc8d6d3ee968cc
Semhylmz <semh-ylmz@hotmail.com>
2024-07-25 15:48:16 +0300

    base code

--- flutter_ble_app/lib/my_app.dart
+++ flutter_ble_app/lib/my_app.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ble_app/theme/app_theme.dart';
-import 'package:flutter_ble_app/views/bluetooth_off_view.dart';
-import 'package:flutter_ble_app/views/scan_view.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_ble_app/views/home_view.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -12,29 +10,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late BluetoothAdapterState _bluetoothAdapterState;
-
-  @override
-  void initState() {
-    _bluetoothAdapterState = BluetoothAdapterState.unknown;
-
-    FlutterBluePlus.adapterState.listen((state) {
-      setState(() {
-        _bluetoothAdapterState = state;
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    Widget currentPage = _bluetoothAdapterState != BluetoothAdapterState.on
-        ? const BleOffPage()
-        : const ScanPage();
-
     return MaterialApp(
       title: 'Flutter BLE App',
-      home: currentPage,
+      home: const HomePage(),
       theme: appTheme,
       debugShowCheckedModeBanner: false,
     );
